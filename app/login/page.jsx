bbab794116e0b8f6ebae6b0373e8c0e51da35013
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-09-03 14:21:58 +0200

    location detection

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,11 +1,13 @@
 "use client";
 import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function page() {
+  const router = useRouter();
   const session = useSession();
 
   if (session.status === "loading") return <p>Loading...</p>;
-  if (session.status === "authenticated") return <p>Authenticated.</p>;
+  if (session.status === "authenticated") return router.push("/");
 
   return (
     <div>
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,10 +1,20 @@
+import axios from "axios";
 import Main from "./Maun";
 import Top from "./Top";
 
-export default function Header() {
+export default async function Header() {
+  let country = await axios
+    .get("https://api.ipregistry.co/?key=b56793nukrwrwq5n")
+    .then((res) => {
+      return res.data.location.country;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   return (
     <div>
-      <Top />
+      <Top country={country} />
       <Main />
     </div>
   );
